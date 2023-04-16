d07c62807b098443f65a7ccd55473eccbe0ea70a
K.Sidorov <2887458@gmail.com>
2023-04-16 11:07:50 +0300

    training python

---
+++ Lesson 1/main.py
@@ -0,0 +1,17 @@
+# a = 5
+# b = 5.89
+# c = 'hello'
+
+# print(f"{a} - {b} - {c}")
+
+# print('Введите первую строку: ')
+# a = input()
+# print(a)
+
+username = input('Введите имя: ')
+if username == 'Маша':
+    print('Ура, это же МАША!')
+elif username == 'Валера':
+    print('Его величество Валерий!')
+else:
+    print(f"Привет, {username}!")
